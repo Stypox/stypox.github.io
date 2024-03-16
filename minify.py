a938e92eb824482cc3cd720e2e97758e081da5d1
Stypox <stypox@pm.me>
2024-03-16 19:49:59 +0100

    Fix some corner cases in minify.py

--- minify.py
+++ minify.py
@@ -10,8 +10,11 @@ with open("styles/style.css", encoding="utf-8") as f:
     css = f.read()
 
 classes = re.findall(r'class=(?:([^ >"]+)|"([^"]+)")', html)
-classes = set(c3 for c1 in classes for c2 in c1 for c3 in c2.split(" "))
-classes.remove("")
+classes = set(c3 for c1 in classes for c2 in c1 for c3 in c2.split())
+try:
+    classes.remove("")
+except KeyError:
+    pass
 classes = sorted(classes, reverse=True)
 classes = {name: gen_name(i) for (i, name) in enumerate(classes)}
 
@@ -20,7 +23,7 @@ for old_name, new_name in classes.items():
 
 def replace_classes(capt):
     capt = capt.group(1) or "" + " " + capt.group(2) or ""
-    capt = capt.strip().split(" ")
+    capt = capt.split()
     capt = [classes[c] for c in capt]
     return "class=\"" + " ".join(capt) + "\""
 html = re.sub(r'class=(?:([^ >"]+)|"([^"]+)")', replace_classes, html)
